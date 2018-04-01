106bedd7d77d6eefb253024b1e3e5a99d4d7f5fd
iamnuno <33864862+iamnuno@users.noreply.github.com>
2018-04-01 09:37:50 +0200

    login UI update - forgot password pane

--- src/com/github/controller/LoginController.java
+++ src/com/github/controller/LoginController.java
@@ -5,6 +5,7 @@ import javafx.animation.*;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -12,8 +13,9 @@ import javafx.util.Duration;
 
 public class LoginController {
 
-    @FXML private Button exitLoginButton, exitRegistrationButton;
-    @FXML private Pane loginPane, registrationPane;
+    @FXML private Button exitLoginButton;
+    @FXML private Pane loginPane, registrationPane, resetPasswordPane;
+    @FXML private Label resetPasswordMsgLabel;
 
     public void initialize() {
 //        DBConnection db = new DBConnection();
@@ -60,5 +62,42 @@ public class LoginController {
                 }
                 registrationPane.setVisible(false);
         }).start();
+    }
+
+    @FXML
+    private void handleForgotPasswordButton() {
+        resetPasswordPane.setVisible(true);
+        resetPasswordPane.setOpacity(0);
+        Timeline timeline = new Timeline();
+        KeyValue kv1 = new KeyValue(loginPane.opacityProperty(), 0);
+        KeyValue kv2 = new KeyValue(resetPasswordPane.opacityProperty(), 1);
+        KeyFrame kf = new KeyFrame(Duration.millis(1000), kv1, kv2);
+        timeline.getKeyFrames().add(kf);
+        timeline.setCycleCount(1);
+        timeline.play();
+    }
+
+    @FXML
+    private void handleExitResetPasswordButton() {
+        Timeline timeline = new Timeline();
+        KeyValue kv1 = new KeyValue(loginPane.opacityProperty(), 1);
+        KeyValue kv2 = new KeyValue(resetPasswordPane.opacityProperty(), 0);
+        KeyFrame kf = new KeyFrame(Duration.millis(1000), kv1, kv2);
+        timeline.getKeyFrames().add(kf);
+        timeline.setCycleCount(1);
+        timeline.play();
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            resetPasswordPane.setVisible(false);
+        }).start();
+    }
+
+    @FXML
+    private void handleResetPasswordButton() {
+        resetPasswordMsgLabel.setText("Further instructions email sent to\nblabla");
     }
 }
