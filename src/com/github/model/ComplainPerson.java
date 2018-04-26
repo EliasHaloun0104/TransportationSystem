f93378a2f36ad43d13956e8ccedc85d0a5f86ce8
msalaheldin <Mohamed.abdelhalem0016@stud.hkr.se>
2018-04-26 11:26:38 +0200

    ComplainPerson class

---
+++ src/com/github/model/ComplainPerson.java
@@ -0,0 +1,43 @@
+package com.github.model;
+
+public class ComplainPerson {
+
+    private String name;
+    private String message;
+    private String date;
+
+    public ComplainPerson(String name, String message, String date) {
+        this.name = name;
+        this.message = message;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
