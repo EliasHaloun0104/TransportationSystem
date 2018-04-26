34ac51646f030ef6acc205224d3fbba4e34d301e
msalaheldin <Mohamed.abdelhalem0016@stud.hkr.se>
2018-04-26 11:37:55 +0200

    Admin - complainsTabSelected, addComplainsToChoice, setComplains, loadComplains

--- src/com/github/controller/AdminController.java
+++ src/com/github/controller/AdminController.java
@@ -1,12 +1,16 @@
 package com.github.controller;
 
 
+import com.github.model.ComplainPerson;
 import com.github.model.DBConnection;
+import javafx.application.Platform;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,18 +23,13 @@ public class AdminController {
     @FXML private TextField tfNumberOfComplains, tfDateOfComplain;
     @FXML private ChoiceBox userNameChoiceBox;
     @FXML private TextArea complainMessagetextArea, answerTextArea;
+    private ArrayList<ComplainPerson> complainPeople = new ArrayList<>();
+
 
 
     public void initialize() {
         buttonFunction = new ButtonFunction(signoutButton);
         buttonFunction.signOutOption();
-        complainsTab.setOnSelectionChanged(
-                e-> {if (complainsTab.isSelected()) {
-                    DBConnection db = new DBConnection(DBConnection.ConnectionType.ADMIN);
-                tfNumberOfComplains.setText(String.valueOf(db.getNumberOfComplains()));
-
-                    }
-        });
 
     }
 
@@ -51,6 +50,54 @@ public class AdminController {
                 }
             }
         }
+    }
+
+    @FXML
+    private void complainsTabSelected(Event event) {
+        DBConnection db = new DBConnection(DBConnection.ConnectionType.ADMIN);
+        tfNumberOfComplains.setText(String.valueOf(db.getNumberOfComplains()));
+
+
+        setComplains(complainPeople);
+
+
+        Platform.runLater(() -> {
+
+
+            userNameChoiceBox.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue) -> {
+
+                tfDateOfComplain.setText(complainPeople.get(newValue.intValue()).getDate());
+                complainMessagetextArea.setText(complainPeople.get(newValue.intValue()).getMessage());
+            });
+
+            loadComplains();
+
+            if (!complainPeople.isEmpty()) {
+                addComplainsToChoice();
+                userNameChoiceBox.getSelectionModel().select(0);
+            }
+
+        });
+
+    }
+
+    private void addComplainsToChoice() {
+
+        userNameChoiceBox.getItems().clear();
+        for (ComplainPerson person : complainPeople) {
+            userNameChoiceBox.getItems().add(person);
+        }
+    }
+
+    public void setComplains(ArrayList<ComplainPerson> complains) {
+        this.complainPeople = complains;
+        addComplainsToChoice();
+    }
+
+    private void loadComplains() {
+        DBConnection db1 = new DBConnection(DBConnection.ConnectionType.ADMIN);
+        complainPeople = db1.getComplain();
+
     }
 }
 
