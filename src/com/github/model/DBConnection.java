a69ed33d57df752250354556fd4c10f7562c9d45
msalaheldin <Mohamed.abdelhalem0016@stud.hkr.se>
2018-05-08 13:09:48 +0200

    admin-viewBooking / added jfoenix library

--- src/com/github/model/DBConnection.java
+++ src/com/github/model/DBConnection.java
@@ -1,5 +1,8 @@
 package com.github.model;
 
+import com.github.controller.Booking;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -603,5 +606,35 @@ public class DBConnection {
             }
         }
 
+    }
+
+    public ObservableList<Booking> getBookings(String sql){
+        ObservableList<Booking> bookings = FXCollections.observableArrayList();
+
+        String query = sql;
+
+        try (PreparedStatement ps = c.prepareStatement(query)) {
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                  bookings.add(new Booking(rs.getString(1),rs.getString(2),rs.getString(3),
+                          rs.getString(4),rs.getString(5),rs.getString(6),
+                          rs.getString(7)));
+
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                c.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+        return bookings;
     }
 }
